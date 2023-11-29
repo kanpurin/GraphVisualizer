fa88907bd3d68db20876126dea5af4d7329d2a33
kanpurin <kanpurin2@gmail.com>
2023-11-30 02:57:37 +0900

    copy周りを見やすく変更

--- src/GraphTextarea.js
+++ src/GraphTextarea.js
@@ -1,11 +1,12 @@
 /* eslint no-undef: 0 */
 import React, { useState, useEffect } from "react";
-import { Button } from 'react-bootstrap'; // React BootstrapからButtonをインポート
+import { Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCopy } from '@fortawesome/free-solid-svg-icons';
+import { faCheckCircle, faClipboard } from '@fortawesome/free-solid-svg-icons';
 
 function GraphTextarea(props) {
   const [textareaValue, setTextareaValue] = useState("");
+  const [copySuccess, setCopySuccess] = useState(false); 
 
   useEffect(() => {
     const updatedTextareaValue = `${props.nodesData.length} ${props.edgesData.length}\n${props.edgesData.map(edge => edge[0]+' '+edge[1]).join('\n')}`;
@@ -16,10 +17,24 @@ function GraphTextarea(props) {
     setTextareaValue(e.target.value); // ユーザーの入力をtextareaValueに反映
   };
 
+  const handleCopy = () => {
+    setCopySuccess(false);
+    navigator.clipboard.writeText(textareaValue)
+      .then(() => {
+        setCopySuccess(true);
+        setTimeout(() => {
+          setCopySuccess(false);
+        }, 1500);
+      })
+      .catch(err => {
+        console.error('Failed to copy: ', err);
+      });
+  };
+
   const handleOutputTextarea = () => {
   };
 
-	return (
+  return (
     <>
       <div className="my-1 position-relative">
         <textarea className="form-control"
@@ -27,8 +42,8 @@ function GraphTextarea(props) {
           onChange={handleTextareaChange}
           style={{ width: '100%', height: '300px' }}
         />
-        <button className="btn btn-primary" onClick={() => navigator.clipboard.writeText(textareaValue)} style={{ position: 'absolute', top: '5px', right: '5px', backgroundColor: 'transparent', border: 'none' }}>
-          <FontAwesomeIcon icon={faCopy} style={{ color: 'gray' }} /> {/* FontAwesomeのコピーのアイコン */}
+        <button className="btn btn-primary" onClick={handleCopy} onMouseEnter={(e) => e.target.style.backgroundColor = 'lightgray'} onMouseLeave={(e) => e.target.style.backgroundColor = 'transparent'} style={{ position: 'absolute', top: '5px', right: '5px', backgroundColor: 'transparent', border: 'none', width: '30px', height: '30px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <FontAwesomeIcon icon={copySuccess ? faCheckCircle : faClipboard} style={{ color: 'gray', fontSize: '1em' }}/>
         </button>
       </div>
       <div className="d-flex justify-content-end">
@@ -37,7 +52,7 @@ function GraphTextarea(props) {
         </Button>
       </div>
     </>
-	)
+  )
 }
 
 export default GraphTextarea;
