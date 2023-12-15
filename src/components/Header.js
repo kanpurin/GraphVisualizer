ba5c3e1e04e378092781f0620d2b947e07b371dd
kanpurin <kanpurin2@gmail.com>
2023-12-16 02:28:38 +0900

    ブログリンクの追加

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,7 @@ function Header() {
 
   const bugReportLink = "https://github.com/kanpurin/GraphVisualizer/issues/new?assignees=&labels=bug&projects=&template=bug_report.md&title=%5BBUG%5D"; // バグ報告用のリンク
   const featureRequestLink = "https://github.com/kanpurin/GraphVisualizer/issues/new?assignees=&labels=enhancement&projects=&template=feature_request.md&title=%5BFeature+Request%5D"; // 機能提案用のリンク
+  const blogLink = "https://kanpurin.hatenablog.com/entry/2023/12/16/022307";
 
   const linkStyle = {
     paddingLeft: "40px" // ここでリンクの字下げを設定
@@ -33,7 +34,8 @@ function Header() {
             <Nav.Link href="#/">Home</Nav.Link>
             <Nav.Link href="#/network-graph" style={linkStyle}>Network Graph</Nav.Link>
             <Nav.Link href="#/2d-graph" style={linkStyle}>2D Graph</Nav.Link>
-            <Nav.Link href="#/about">About</Nav.Link>
+            {/* <Nav.Link href="#/about">About</Nav.Link> */}
+            <Nav.Link href={blogLink}>About</Nav.Link>
             <Nav.Link href={bugReportLink}>Report a Bug</Nav.Link>
             <Nav.Link href={featureRequestLink}>Suggest a Feature</Nav.Link>
           </Nav>
